e6083b19981656c9de81a317c3b3f2047fdd68d5
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2017-11-06 15:48:08 -0500

    Update license and move files to src

--- src/package.lisp
+++ src/package.lisp
@@ -1,22 +1,12 @@
-;;;Copyright (c) 2013 Wilfredo Velázquez-Rodríguez
+;;;win32 - CFFI bindings to Win32 API
+;;;Written in 2013 by Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
 ;;;
-;;;This software is provided 'as-is', without any express or implied
-;;;warranty. In no event will the authors be held liable for any damages
-;;;arising from the use of this software.
-;;;
-;;;Permission is granted to anyone to use this software for any purpose,
-;;;including commercial applications, and to alter it and redistribute
-;;;it freely, subject to the following restrictions:
-;;;
-;;;1. The origin of this software must not be misrepresented; you must not
-;;;   claim that you wrote the original software. If you use this software
-;;;   in a product, an acknowledgment in the product documentation would
-;;;   be appreciated but is not required.
-;;;
-;;;2. Altered source versions must be plainly marked as such, and must not
-;;;   be misrepresented as being the original software.
-;;;
-;;;3. This notice may not be removed or altered from any source distribution.
+;;;To the extent possible under law, the author(s) have dedicated all copyright
+;;;and related and neighboring rights to this software to the public domain
+;;;worldwide. This software is distributed without any warranty.
+;;;You should have received a copy of the CC0 Public Domain Dedication along
+;;;with this software. If not, see
+;;;<http://creativecommons.org/publicdomain/zero/1.0/>.
 
 (defpackage #:win32
   (:use #:cffi #:cl)
