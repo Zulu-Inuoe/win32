0b38fb2bb1ea5a90c9408c8c176990f1eb38b8b8
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-02-03 09:11:52 -0500

    Remove license headers. Tracking via LICENSE is enough

--- src/win32.lisp
+++ src/win32.lisp
@@ -1,13 +1,3 @@
-;;;win32 - CFFI bindings to Win32 API
-;;;Written in 2013 by Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
-;;;
-;;;To the extent possible under law, the author(s) have dedicated all copyright
-;;;and related and neighboring rights to this software to the public domain
-;;;worldwide. This software is distributed without any warranty.
-;;;You should have received a copy of the CC0 Public Domain Dedication along
-;;;with this software. If not, see
-;;;<http://creativecommons.org/publicdomain/zero/1.0/>.
-
 (in-package #:win32)
 
 (define-foreign-library version
